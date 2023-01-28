e61a9e764a8fbbc375ae49e34cc0d4f19218b2d6
amos-kibet <kibetamos97@gmail.com>
2023-01-28 19:14:06 +0300

    refactor tests

--- test/slaclone_web/live/user_reset_password_live_test.exs
+++ test/slaclone_web/live/user_reset_password_live_test.exs
@@ -43,7 +43,7 @@ defmodule SlacloneWeb.UserResetPasswordLiveTest do
           user: %{"password" => "secret12", "confirmation_password" => "secret123456"}
         )
 
-      assert result =~ "should be at least 12 character"
+      assert result =~ "should be at least 4 character"
       assert result =~ "does not match password"
     end
   end
@@ -82,7 +82,7 @@ defmodule SlacloneWeb.UserResetPasswordLiveTest do
         |> render_submit()
 
       assert result =~ "Reset Password"
-      assert result =~ "should be at least 12 character(s)"
+      assert result =~ "should be at least 4 character(s)"
       assert result =~ "does not match password"
     end
   end
